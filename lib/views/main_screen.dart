70c4075fd4330c73cd2a32e3c2ef87c24e07c45f
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-17 22:28:27 +0700

    SideBar Final - Switch

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_admin_scaffold/admin_scaffold.dart';
+import 'side_bar_screens/vendors_screen.dart';
+import 'side_bar_screens/buyers_screen.dart';
+import 'side_bar_screens/category_screen.dart';
+import 'side_bar_screens/orders_screen.dart';
+import 'side_bar_screens/products_screen.dart';
+import 'side_bar_screens/upload_banner_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -10,6 +16,54 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  Widget _selectedScreen = VendorsScreen();
+  screenSelector(item) {
+    switch (item.route) {
+      case BuyersScreen.id:
+        setState(() {
+          _selectedScreen = BuyersScreen();
+        });
+        break;
+
+      case VendorsScreen.id:
+        setState(() {
+          _selectedScreen = VendorsScreen();
+        });
+        break;
+
+      case OrdersScreen.id:
+        setState(() {
+          _selectedScreen = OrdersScreen();
+        });
+        break;
+
+      case CategoryScreen.id:
+        setState(() {
+          _selectedScreen = CategoryScreen();
+        });
+        break;
+
+      case UploadBannerScreen.id:
+        setState(() {
+          _selectedScreen = UploadBannerScreen();
+        });
+        break;
+
+      case ProductsScreen.id:
+        setState(() {
+          _selectedScreen = ProductsScreen();
+        });
+        break;
+
+      default:
+        {
+          setState(() {
+            _selectedScreen = VendorsScreen();
+          });
+        }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return AdminScaffold(
@@ -17,43 +71,42 @@ class _MainScreenState extends State<MainScreen> {
         backgroundColor: Colors.blue,
         title: const Text("Management"),
       ),
-      body: const Text(
-        "Dashboard",
-      ),
-      sideBar: const SideBar(
-        items: [
+      body: _selectedScreen,
+      sideBar: SideBar(
+        items: const [
           AdminMenuItem(
             title: 'Vendors',
-            route: "",
+            route: VendorsScreen.id,
             icon: CupertinoIcons.person_3,
           ),
           AdminMenuItem(
             title: 'Buyers',
-            route: "",
+            route: BuyersScreen.id,
             icon: CupertinoIcons.person,
           ),
           AdminMenuItem(
             title: 'Orders',
-            route: "",
+            route: OrdersScreen.id,
             icon: CupertinoIcons.shopping_cart,
           ),
           AdminMenuItem(
             title: 'Categories',
-            route: "",
+            route: CategoryScreen.id,
             icon: Icons.category,
           ),
           AdminMenuItem(
             title: 'Upload Banners',
-            route: "",
+            route: UploadBannerScreen.id,
             icon: Icons.upload,
           ),
           AdminMenuItem(
             title: 'Product',
-            route: "",
+            route: ProductsScreen.id,
             icon: Icons.store,
           ),
         ],
-        selectedRoute: '',
+        selectedRoute: VendorsScreen.id,
+        onSelected: (item) => screenSelector(item),
       ),
     );
   }
