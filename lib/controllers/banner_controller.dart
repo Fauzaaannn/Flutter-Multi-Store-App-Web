605459e04d2b636efe62894ecfa2008c1241e0a6
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-23 16:37:22 +0700

    Retrive Banners From API

--- lib/controllers/banner_controller.dart
+++ lib/controllers/banner_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:app_web/global_variables.dart';
 import 'package:app_web/models/banner.dart';
 import 'package:app_web/services/manage_http_response.dart';
@@ -35,6 +37,36 @@ class BannerController {
       );
     } catch (e) {
       print(e);
+    }
+  }
+
+  // fetch banners
+  Future<List<BannerModel>> loadBanners() async {
+    try {
+      // send a http get request to fetch banners
+      http.Response response = await http.get(
+        Uri.parse('$uri/api/banner'),
+        headers: <String, String>{
+          "Content-Type": "application/json; charset=UTF-8"
+        },
+      );
+
+      print(response.body);
+
+      if (response.statusCode == 200) {
+        // OK
+        List<dynamic> data = jsonDecode(response.body);
+
+        List<BannerModel> banners =
+            data.map((banner) => BannerModel.fromJson(banner)).toList();
+
+        return banners;
+      } else {
+        // throw an exception if the server responsed with an error status code
+        throw Exception('Failed to load banners');
+      }
+    } catch (e) {
+      throw Exception('Failed to load banners $e');
     }
   }
 }
--- lib/models/banner.dart
+++ lib/models/banner.dart
@@ -14,14 +14,14 @@ class BannerModel {
     };
   }
 
+  String toJson() => json.encode(toMap());
+
   factory BannerModel.fromMap(Map<String, dynamic> map) {
     return BannerModel(
       id: map['_id'] as String,
       image: map['image'] as String,
     );
   }
-
-  String toJson() => json.encode(toMap());
 
   factory BannerModel.fromJson(String source) =>
       BannerModel.fromMap(json.decode(source) as Map<String, dynamic>);
