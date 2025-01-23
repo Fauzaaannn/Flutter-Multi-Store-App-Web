4c024af97ba035b1bbcb153e4970840c7d9cb951
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-23 13:35:31 +0700

    Banner Screen UI

--- lib/views/side_bar_screens/upload_banner_screen.dart
+++ lib/views/side_bar_screens/upload_banner_screen.dart
@@ -1,13 +1,95 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-class UploadBannerScreen extends StatelessWidget {
-  static const String id = '\banner-screen';
+class UploadBannerScreen extends StatefulWidget {
+  static const String id = 'UploadBannerScreen';
   const UploadBannerScreen({super.key});
 
   @override
+  State<UploadBannerScreen> createState() => _UploadBannerScreenState();
+}
+
+class _UploadBannerScreenState extends State<UploadBannerScreen> {
+  dynamic _image;
+
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+    );
+
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Upload Banner Screen"),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Banners',
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        Divider(
+          color: Colors.grey,
+          thickness: 2,
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(8.0, 0.0, 0.0, 0.0),
+          child: Row(
+            children: [
+              Container(
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Center(
+                  child: _image != null
+                      ? Image.memory(_image)
+                      : Text(
+                          'Category image',
+                        ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: Text(
+                    'Save',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20.0, 8.0, 0.0, 0.0),
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('Pick Image'),
+          ),
+        ),
+      ],
     );
   }
 }
