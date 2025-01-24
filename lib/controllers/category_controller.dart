4a476be03b01f7e3d6a9b1b4bdec4d7c749c5dcc
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-01-24 14:33:58 +0700

    retrieve categories from API

--- lib/controllers/category_controller.dart
+++ lib/controllers/category_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:app_web/global_variables.dart';
 import 'package:app_web/models/category.dart';
 import 'package:app_web/services/manage_http_response.dart';
@@ -50,6 +52,33 @@ class CategoryController {
       );
     } catch (e) {
       print('Error Uploading to Cloudinary: $e');
+    }
+  }
+
+  // load the uploaded category
+  Future<List<Category>> loadCategories() async {
+    try {
+      // send a http get request to load the categories
+      http.Response response = await http.get(
+        Uri.parse('$uri/api/categories'),
+        headers: <String, String>{
+          "Content-Type": "application/json; charset=UTF-8"
+        },
+      );
+
+      print(response.body);
+
+      if (response.statusCode == 200) {
+        // OK
+        final List<dynamic> data = jsonDecode(response.body);
+        List<Category> categories =
+            data.map((category) => Category.fromJson(category)).toList();
+        return categories;
+      } else {
+        throw Exception('Failed to load categories');
+      }
+    } catch (e) {
+      throw Exception('Error to load categories: $e');
     }
   }
 }
